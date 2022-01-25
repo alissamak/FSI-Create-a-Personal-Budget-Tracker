033ae2582068b00e7a73924064fea2126bd13150
Alissa <alissamak@outlook.com>
2022-01-24 20:23:43 -0500

    added function and non working while loops

--- index.js
+++ index.js
@@ -3,10 +3,26 @@ let monthlyExpenses = 0
 let annualExpenses = 0
 
 // Your Code Here
-//alternative
-//let stringAnswer = window.prompt(weeklyExpenseQuestions[0])
-//let numberAnswer = parseFloat(stringAnswer) --converts string into number
-for (let i = 0; i < weeklyExpenseQuestions.length; i++){
+/*alternative to let answer = parseFloat(window.prompt(weeklyExpenseQuestions[0]))
+let stringAnswer = window.prompt(weeklyExpenseQuestions[0])
+let numberAnswer = parseFloat(stringAnswer) --converts string into number*/
+
+//function
+weeklyExpenses = askQuestions(weeklyExpenseQuestions)
+monthlyExpenses = askQuestions(monthlyExpenseQuestions)
+annualExpenses = askQuestions(annualExpenseQuestions)
+
+function askQuestions(listOfQuestions){
+    let expenses = 0
+    for (let i = 0; i <listOfQuestions.length; i++){
+        let answer = parseFloat(window.prompt(listOfQuestions[i]))
+        expenses = expenses + answer
+    }
+    return expenses
+}
+
+//for loops
+/*for (let i = 0; i < weeklyExpenseQuestions.length; i++){
     let answer = parseFloat(window.prompt(weeklyExpenseQuestions[i]))
     weeklyExpenses = weeklyExpenses + answer
 }
@@ -17,4 +33,24 @@ for (let i = 0; i < monthlyExpenseQuestions.length; i++){
 for (let i = 0; i < annualExpenseQuestions.length; i++){
     let answer = parseFloat(window.prompt(annualExpenseQuestions[i]))
     annualExpenses = annualExpenses + answer
+}*/
+
+//while loops
+/*let i = 0
+while (i < weeklyExpenseQuestions.length){
+    let answer = parseFloat(window.prompt(weeklyExpenseQuestions[i]));
+    i++;
+    weeklyExpenses = weeklyExpenses + answer;
 }
+
+while (i < monthlyExpenseQuestions.length){
+    let answer = parseFloat(window.prompt(monthlyExpenseQuestions[i]));
+    i++;
+    monthlyExpenses = monthlyExpenses + answer;
+}
+
+while (i < annualExpenseQuestions.length){
+    let answer = parseFloat(window.prompt(annualExpenseQuestions[i]));
+    i++;
+    annualExpenses = annualExpenses + answer;
+}*/
